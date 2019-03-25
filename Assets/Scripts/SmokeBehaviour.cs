4f1295ff9dec2232b5f1b7ceb0fec4aee073b495
Dmitrii <loki.di90@gmail.com>
2019-03-25 17:52:25 +0000

    Add smoke script

---
+++ Assets/Scripts/SmokeBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeBehaviour : MonoBehaviour
+{
+    public float idleSpeed;
+    public float idleSize;
+    public float idleRate;
+
+    public float maxSpeed=4.0f;
+    public float maxSize=1.0f;
+    public float maxRate=15.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
