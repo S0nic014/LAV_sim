40344431894048fdafa0bee9c930c30ee4dc312b
S0nic014 <loki.di90@gmail.com>
2019-04-23 21:25:58 +0100

    Shooting implemeted

--- Assets/Scripts/LavFire.cs
+++ Assets/Scripts/LavFire.cs
@@ -8,6 +8,12 @@ public class LavFire : MonoBehaviour
     public AudioSource fireSourse;
     public GameObject barrel;
     private Animator anim;
+    public float damage=10f;
+    public float range=100f;
+    public float impactForce=30f;
+    public Camera gunnerCam;
+    public ParticleSystem muzzleSmoke;
+    public GameObject impactEffect;
 
     void Start() {
         fireSourse.clip=fireSound;
@@ -31,7 +37,32 @@ public class LavFire : MonoBehaviour
             {
                 fireSourse.Play();
                  anim.Play("FireAnim");
+                 CastRay();
             }   
     }
      
+    public void CastRay()
+    {
+        muzzleSmoke.Play();
+        RaycastHit hit;
+
+        if (Physics.Raycast(gunnerCam.transform.position, gunnerCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            TargetBehaviour target = hit.transform.GetComponent<TargetBehaviour>();
+            if (target!=null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody!=null)
+            {
+                hit.rigidbody.AddForce(hit.normal*impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
 }
