10a5a2d20aa06867ca67c6d72f170bcabadc091a
S0nic014 <loki.di90@gmail.com>
2019-04-29 17:48:14 +0100

    Enemy explosion effect

--- Assets/Scripts/TargetBehaviour.cs
+++ Assets/Scripts/TargetBehaviour.cs
@@ -5,6 +5,7 @@ public class TargetBehaviour : MonoBehaviour
     public float health=100f;
     public GameObject turret;
     public GameObject destroyedModel;
+    public GameObject explosionPrefab;
 
     public void TakeDamage(float amount)
     {
@@ -24,8 +25,9 @@ public class TargetBehaviour : MonoBehaviour
             Vector3 turretPosition= new Vector3(deadPrefab.transform.position.x, deadPrefab.transform.position.y+1f, deadPrefab.transform.position.z);
             GameObject deadTurret = Instantiate(turret, turretPosition, deadPrefab.transform.rotation);
             Rigidbody turretRb=deadTurret.GetComponent<Rigidbody>();
-            turretRb.AddForce(200, 2000, 200);
-            //turretRb.AddExplosionForce
+            turretRb.AddExplosionForce(2000f, deadPrefab.transform.position, 100f, 100f, ForceMode.Force);
+            GameObject explosion = Instantiate( explosionPrefab ,deadPrefab.transform.position, deadPrefab.transform.rotation);
+            Destroy(explosion, 10f);
         }
 
         Destroy(gameObject);
